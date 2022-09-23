697a3f7eaed3d240a6ddf636b390d7c7abd92842
malesoli <malenamasoli@gmail.com>
2022-09-22 23:20:03 -0300

    subo archivos

---
+++ js/variables.js
@@ -0,0 +1,49 @@
+numero=100
+console.log(numero)
+
+nombre="Lola"
+console.log(typeof nombre)
+estado=true
+console.log(typeof estado)
+peso=12.5
+console.log(typeof peso )
+
+numero=580
+console.log(numero)
+
+var num1 // declaracion de variable 
+console.log(num1)
+//declarando e inicializando la variable 
+var num2=45
+console.log(num2)
+
+const IVA=21.0
+
+var numeroOb=new Number(123)
+console.log(numeroOb)
+
+
+var valor='50'
+console.log(typeof valor)//string
+valorNuevo=Number.parseInt(valor)
+console.log(typeof valorNuevo)//number
+
+
+var numeroA=100
+var numeroB=3
+var div=numeroA/numeroB
+
+console.log(div)
+
+var resto=numeroA%3
+
+
+console.log(resto)
+//+= -= *= /=
+
+document.write("HOLA DESDE EL HTML" + "<br>" )
+
+var somevar = "foo";
+for(i=1;i<4;i++){
+document.write(i+". "+somevar+"<br>");
+}
